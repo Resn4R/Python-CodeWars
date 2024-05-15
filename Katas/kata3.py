667c0399bedf4855a2b50d231f8bad93e598f2cd
Vito <purview-joust.0x@icloud.com>
2024-05-15 23:10:14 +0200

    kata3

---
+++ Katas/kata3.py
@@ -0,0 +1,42 @@
+'''
+Rock Paper Scissors
+
+Let's play! You have to return which player won! In case of a draw return Draw!.
+
+Examples(Input1, Input2 --> Output):
+
+"scissors", "paper" --> "Player 1 won!"
+"scissors", "rock" --> "Player 2 won!"
+"paper", "paper" --> "Draw!"
+'''
+
+def rps(p1, p2):
+    match p1:
+        case "scissors":
+            match p2:
+                case "scissors":
+                    return "Draw!"
+                case "paper":
+                    return "Player 1 won!"
+                case "rock":
+                    return "Player 2 won!"
+                case _: return
+        case "paper":
+            match p2:
+                case "paper":
+                    return "Draw!"
+                case "rock":
+                    return "Player 1 won!"
+                case "scissors":
+                    return "Player 2 won!"
+                case _: return
+        case "rock":
+            match p2:
+                case "rock":
+                    return "Draw!"
+                case "scissors":
+                    return "Player 1 won!"
+                case "paper":
+                    return "Player 2 won!"
+                case _: return
+        case _: return
