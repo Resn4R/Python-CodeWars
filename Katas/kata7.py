6f540d3bf6d099ec9ef68ccd89f1794436740303
Vito <purview-joust.0x@icloud.com>
2024-05-31 15:42:26 +0100

    refactor

--- Katas/kata7.py
+++ Katas/kata7.py
@@ -1,7 +1,9 @@
 def stringCalculator (numbers: str) -> int:
     return sum(
         list(
-            map(int, list( 
+            map(
+                int, 
+                list( 
                     filter(None, numbers.split(','))
                     )
                 )
