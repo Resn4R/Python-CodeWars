a21b09521fd1261854120f8b530dfdd119d9200e
Vito <purview-joust.0x@icloud.com>
2024-06-01 23:14:58 +0100

    cs50

--- Katas/cs50_random.py
+++ Katas/cs50_random.py
@@ -1,4 +1,9 @@
-from random import choice
+import random
 
-coin = choice(["heads", "tails"])
+coin = random.choice(["heads", "tails"])
 print (coin)
+
+#or can just write print(choice((["heads", "tails"]))
+
+randomInt = random.randint(1,10)
+print (randomInt)
